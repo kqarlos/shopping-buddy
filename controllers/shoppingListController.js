204a1fea7d59e0438e8969fa7b294fa527a59413
kqarlos <kqarlos@hotmail.com>
2020-01-21 19:29:01 -0800

    Controller creates the routes to get all, create and update an item

--- controllers/shoppingListController.js
+++ controllers/shoppingListController.js
@@ -1,15 +1,30 @@
 var express = require("express");
-var burger = require("../models/todo.js");
+var shoppingList = require("../models/shoppinglist.js");
 
 var router = express.Router();
 
-router.get("/api/todo", function(req, res) {
-
+router.get("/api/list", function (req, res) {
+  shoppingList.all(function (data) {
+    console.log(data);
+    res.render("index", { list: data });
   });
-  
-  router.post("/api/todo", function(req, res) {
 
+});
 
+router.post("/api/list", function (req, res) {
+  shoppingList.create("item, done", `${req.body.item}, ${req.body.done}`, function (result) {
+    res.json({ id: result.insertID });
   });
+});
+
+router.put("/api/list/:id", function (req, res) {
+  shoppingList.update("done = " + req.body.done, "id=" + req.params.id, function (result) {
+    if (result.changedRows == 0) {
+      return res.status(404).end();
+    } else {
+      res.status(200).end();
+    }
+  });
+});
 
 module.exports = router;
