7cde267469005bd969545c05f509df97a86e7b11
kqarlos <kqarlos@hotmail.com>
2020-01-22 12:19:59 -0800

    Adds functionality to add to list button

---
+++ public/assets/js/script.js
@@ -0,0 +1,55 @@
+$(function () {
+
+    getList();
+    $("#add").on("click", addToList);
+    $(document).on("click", "#cart", addToCart);
+    $(document).on("click", "#del", deleteItem);
+
+
+    function getList() {
+
+        // This function grabs posts from the database and updates the view
+
+        $.get("/api/list", function (data) {
+            console.log("All items", data);
+        });
+
+
+
+    }
+
+    // Creates a new item and sends it to the route. Reload Page
+    function addToList() {
+
+        var item = {
+            item: $("#newItem").val().trim(),
+            done: false
+        };
+
+        // Send the POST request.
+        $.ajax("/api/list", {
+            type: "POST",
+            data: item
+        }).then(
+            function () {
+                console.log("created new item");
+                location.reload();
+            }
+        );
+
+
+
+
+    }
+
+    function addToCart() {
+
+
+    }
+    function deleteItem() {
+
+
+    }
+}
+
+});
